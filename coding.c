e751d4a471e156c2f455e3d63aa9a7a7fa0b815e
Adrian Colcer <adrian.colcer1@gmail.com>
2026-03-02 00:50:16 +0000

    Researching range encoding technique. Specification included from official LZMA repo.

---
+++ coding.c
@@ -0,0 +1,30 @@
+// naive implementation of range coding
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+int code = 0;
+int low = 0;
+int range = 100000;
+
+/*
+* Contextual Probability
+2^11 = 2048 ()
+
+*/
+uint16_t Cprob = ;
+
+void AppendDigit()
+{
+
+}
+
+int main()
+{
+    char string[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.";
+
+
+
+    return 0
+}
